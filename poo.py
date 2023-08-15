d9b3799fa9fed9812043f26ac985c921f5ffacc3
Arthur Antunes <51514914+Arthur99Silva@users.noreply.github.com>
2023-08-15 14:57:12 -0300

    Add files via upload

---
+++ poo.py
@@ -0,0 +1,33 @@
+"""
+Programação Orientada a Objetos - POO
+
+- É um paradigma de programação que utiliza de mapeamento de objetos do mundo real para modelos computacionais;
+
+- Paradigma de programação é a forma/metodologia utilizada para pensar/desenvolver sistemas;
+
+Principais elementos da Orientação a Objetos:
+
+- Classe -> Modelo do objeto do mundo real sendo representado computacionalmente;
+- Atributo -> Características do objeto.
+- Método Comportamento do objeto (funções) .
+- Construtor -> Método especiat uti Zizado para criar os objetos;
+- Objeto -> Instância da classe.
+
+"""
+numero = 10
+print(numero)
+print(type(numero))
+
+nome = 'Arthur'
+print(nome)
+print(type(nome))
+
+
+class Produto:
+    pass
+
+
+ps4 = Produto()
+print(ps4)
+print(type(ps4))
+
