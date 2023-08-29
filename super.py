ffa870c75c98a7a58f7cc719b053dd735e4da1dc
Arthur Antunes <51514914+Arthur99Silva@users.noreply.github.com>
2023-08-29 09:32:32 -0300

    Add files via upload

---
+++ super.py
@@ -0,0 +1,29 @@
+"""
+O método super()
+
+
+
+"""
+
+
+class Animal:
+    def __init__(self, nome, especie):
+        self.__nome = nome
+        self.__especie = especie
+
+    def faz_som(self, som):
+        print(f'O {self.__nome} faz {som}')
+
+
+class Gato(Animal):
+    def __init__(self, nome, especie, raca):
+        #Animal.__init__(self, nome, especie)
+        super().__init__(nome, especie)
+        self.__raca = raca
+
+
+felix = Gato('Felix', 'Felino', 'Angora')
+
+felix.faz_som('miau')
+
+
