17d66018f9e0333f31d9df650a784b445e30876f
Arthur Antunes <51514914+Arthur99Silva@users.noreply.github.com>
2023-08-29 10:50:24 -0300

    Add files via upload

---
+++ mro.py
@@ -0,0 +1,53 @@
+"""
+POO - Method Resolution Order - MRO
+
+Method Resolution Order (Resolução de Ordem de Métodos), e a ordem
+de execucâo dos métodos (quem será executado primeiro) .
+
+Em Python, a gente pode conferir a ordem de execução dos métodos (MRO) de 3 formas:
+    - Via propriedade da classe
+    - Via método mro()
+    - Via help
+
+"""
+
+
+class Animal:
+    def __init__(self, nome):
+        self.__nome = nome
+
+    def cumprimentar(self):
+        return f'Eu sou o {self.__nome}'
+
+
+class Aquatico(Animal):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def nadar(self):
+        return f'{self._Animal__nome} esta nadando...'
+
+    def cumprimentar(self):
+        return f'Eu sou o {self._Animal__nome} do mar'
+
+
+class Terrestre(Animal):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def andar(self):
+        return f'{self._Animal__nome} esta andando...'
+
+    def cumprimentar(self):
+        return f'Eu sou o {self._Animal__nome} da terra'
+
+
+class Pinguim(Aquatico, Terrestre):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+
+tux = Pinguim('Tux')
+print(tux.nadar())
+print(tux.andar())
+print(tux.cumprimentar()) # Method Resolution Order
